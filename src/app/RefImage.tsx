a75bde15dea69b3c535c33f8782cdd9ffc6703d3
unknown <132866503+pianissi@users.noreply.github.com>
2024-04-17 17:57:32 +1000

    feat: initial functionality aka multiple resizeable images

--- src/app/RefImage.tsx
+++ src/app/RefImage.tsx
@@ -1,25 +1,102 @@
 "use client";
 
-import { Rnd } from "react-rnd";
+import { DraggableData, Position, ResizableDelta, Rnd } from "react-rnd";
 import Image from "next/image";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
+import classNames from "classnames";
+import { useOnClickOutside } from "usehooks-ts";
 
-export default function RefImage({src}: { src: string }) {
+export default function RefImage({id, src, onDragStart, onDragStop, scale, onDelete}: { id:number, src: string, onDragStart: any, onDragStop: any, scale: number, onDelete: any}) {
+  const [width, setWidth] = useState(500);
+  const [height, setHeight] = useState(50);
+  const [isRatioLocked, setIsRatioLocked] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+  const imageRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKey);
+
+    window.addEventListener('keyup', handleKey);
+  }, []);
+
+  useOnClickOutside(imageRef, handleUnSelection);
+
+  function handleKey(event : any) {
+    setIsRatioLocked(event.shiftKey);
+    if (event.key === "Delete") {
+      handleDelete(event);
+    }
+  }
+
+  // TODO make only selected image deleted
+  function handleDelete(event: any) {
+    
+    onDelete(id);
+  }
+
+  function handleOnLoad(event : any) {
+    if (imageRef.current === null) {
+      return;
+    }
+    setHeight(imageRef.current.naturalHeight);
+    setWidth(imageRef.current.naturalWidth);
+  }
+
+  function handleOnDragStart(event : any) {
+    onDragStart(event);
+    setIsSelected(true);
+  }
+
+  function handleOnDragStop(event : any) {
+    onDragStop(event);
+    setIsSelected(true);
+  }
+
+  function handleSelection(event : any) {
+    setIsSelected(true);
+  }
+
+  function handleUnSelection(event : any) {
+    setIsSelected(false);
+  }
+
+  function handleOnResizeStop(event: any, dir: any, ref: HTMLElement, delta: ResizableDelta, position: Position) {
+    setWidth(parseInt(ref.style.width));
+    setHeight(parseInt(ref.style.height));
+  }
+
+  // TODO: fix selection on click
   return (
     <Rnd
       default={{
         x: 0,
         y: 0,
-        width: 320,
-        height: 200,
+        width: width,
+        height: height,
       }}
+      size={{width: width, height: height}}
+      lockAspectRatio={isRatioLocked}
+      onResizeStop={handleOnResizeStop}
+      onResize={handleSelection}
+      onDragStart={handleOnDragStart}
+      onDrag={handleSelection}
+      onDragStop={handleOnDragStop}
+      onMouseDown={handleSelection}
+      scale={scale}
+      className={classNames("border-4",
+        {
+          "border-black": !isSelected,
+          "border-sky-600": isSelected
+        }
+      )}
     >
       <Image 
         src={src}
+        ref={imageRef}
         fill={true}
         alt="user reference image"
         draggable={false}
+        onLoad={handleOnLoad}
       />
     </Rnd>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,147 @@
 'use client';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RefImage from "./RefImage";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface ImagePropType {
+  fileUrl: string;
+  id: number;
+}
+
+const acceptedFiles = [
+  "image/png",
+  "image/jpeg",
+  "image/webp",
+  "image/gif"
+];
 
 export default function Home() {
-  const [file, setFile] = useState<Blob>()
-  const [fileUrl, setFileUrl] = useState("")
-  const [fileSubmitted, setFileSubmitted] = useState(false)
+  const [fileTransfer, setFileTransfer] = useState<Blob[]>();
+  const [imageProps, setImageProps] = useState<ImagePropType[]>([]);
+  const [isImageDragging, setIsImageDragging] = useState(true);
+  const [idCount, setIdCount] = useState(0);
+  const [scale, setScale] = useState(1);
 
-  function handleChange(event : any) {
-    setFile(event.target.files[0])
-  }
+  useEffect(() => {
+    console.log(fileTransfer);
+    if (fileTransfer === undefined) {
+      return;
+    }
 
-  function handleSubmit(event : any) {
-    const reader = new FileReader();
-    reader.addEventListener('load', (e) => {
-      if (e.target instanceof FileReader) {
-        if (typeof e.target.result === "string") {
-          setFileUrl(e.target.result);
-          setFileSubmitted(true)
-        }
-      } 
-    });
-    if (file instanceof Blob) {  
-      reader.readAsDataURL(file);
-  
+    var i = 0;
+
+    while (i < fileTransfer.length) {
+      var file = fileTransfer[i];
+      i++;
+      if (!(acceptedFiles.includes(file.type))) {
+        continue;
+      }
+      const reader = new FileReader();
+
+      // TODO  fix dragging multiple files
+      reader.addEventListener('load', (e) => {
+        if (e.target instanceof FileReader) {
+          if (typeof e.target.result === "string") {
+            setImageProps([...imageProps, {fileUrl: e.target.result, id:idCount}]);
+            setIdCount(idCount + 1);
+            setIsImageDragging(false);
+          }
+        } 
+      });
+
+      if (file instanceof Blob) {
+        reader.readAsDataURL(file);
+        console.log("new image");
+      }
+
+      
     }
     
+  }, [fileTransfer]);
+
+  function handleOnDrop(event : any) {
+    event.stopPropagation();
+    event.preventDefault();
+    const {files} = event.dataTransfer;
+    setFileTransfer(files);
   }
 
+  function handleOnDragOver(event : any) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  function handleImageOnDragStart(event : any) {
+    setIsImageDragging(true);
+  }
+
+  function handleImageOnDragEnd(event : any) {
+    setIsImageDragging(false);
+  }
+
+  function handleOnTransform(event : any){
+    setScale(event.instance.transformState.scale); // output scale factor
+  }
+
+  function handleOnDelete(id : number){
+    console.log("id")
+    setImageProps(imageProps.filter((imageProp) => imageProp.id !== id)); // output scale factor
+  }
+  // Order  of array is the arrangement of files
+
   return (
-    <div>
-      <input type="file" onChange={handleChange}/>
-      <button type="button" onClick={handleSubmit}>upload</button>
-      {fileSubmitted ? (
-        <RefImage src={fileUrl}/>
-      ) : (<div></div>)}
+    <div 
+      id="drop_zone"
+      className="bg-zinc-700 h-screen w-screen"
+      onDrop={handleOnDrop}
+      onDragOver={handleOnDragOver}
+      >
+      <TransformWrapper limitToBounds={false} disabled={isImageDragging} minScale={0.20} maxScale={10} onTransformed={handleOnTransform}>
+        <TransformComponent>
+          <div id="wrapper" className="h-screen w-screen">
+            <AnimatePresence>
+              {(imageProps.length === 0) && (
+                <motion.div
+                  initial={{ scale: 0.5, y: 300, opacity: 0 }}
+                  animate={{ scale: 1, y: 0, opacity: 1 }}
+                  exit={{ scale: 0.5, y: 300, opacity: 0 }}
+                  transition={{
+                    type: "spring",
+                    duration: 0.5,
+                    delay: 0,
+                    bounce: 0.2
+                  }}
+                >
+                  <p className="h-screen flex items-center justify-center">Drag and Drop your Images!</p>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            {imageProps.map(imageProp => 
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 0.8,
+                  delay: 0.5,
+                  ease: [0, 0.71, 0.2, 1.01]
+                }}
+              >
+                <RefImage 
+                  key={imageProp.id} 
+                  id={imageProp.id}
+                  src={imageProp.fileUrl}
+                  scale={scale}
+                  onDragStart={handleImageOnDragStart}
+                  onDragStop={handleImageOnDragEnd}
+                  onDelete={handleOnDelete}
+                ></RefImage>
+              </motion.div>
+            )}
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 }
